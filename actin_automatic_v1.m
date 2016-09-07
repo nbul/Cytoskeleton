e0c7c2c4824552a457807e10b60bdb4994a15009
nbul <nbul001@gmail.com>
2016-09-07 21:45:42 +0100

    Automation
    Added selection of folder with data, corrected warnings and improved
    comments, replaced “actin” with “cytoskeleton”

--- actin_automatic_v1.m
+++ actin_automatic_v1.m
@@ -3,55 +3,74 @@
 % Adapted to use Sobel 5x5 to find direction
 % Check analysis of Microtubule density
 
-clear all
+clc
+clear variables
 close all
+%% Determening paths and setting folders
+currdir = pwd;
+addpath(pwd);
+filedir = uigetdir();
+cd(filedir);
+%Folders with images
+ck_dir =[filedir, '/cytoskeleton']; 
+b_dir = [filedir, '/borders'];
 
-cd('../../');
-cd('actin/');
+%Folder to save information about cells
+mkdir(filedir,'/distribution');
+dist_dir = [filedir, '/distribution'];
+
+mkdir(filedir,'/images_analysed');
+im_dir = [filedir, '/images_analysed'];
+
+mkdir(filedir,'/summary');
+sum_dir = [filedir, '/summary'];
+%% Number of files to analyse
+cd(ck_dir);
 files = dir('*.tif');
-cd('../');
-mkdir('distribution');
-mkdir('summary');
-mkdir('images_analysed');
+cd(currdir);
+
+%% Parameters
+bin_size = 4;
+binrange = -90 : bin_size : 90;
+bincenter=binrange(1:(end-1)) + bin_size/2;
+
 for loop=1:length(files);
-    cd('actin/');
+    %% reading files
+    cd(ck_dir);
     clear Name Number Actin_file Image_actin Path  Image_borders
     Name = files(loop).name;
     Number = sscanf(Name, '%f');
     Actin_file = [num2str(Number),'.tif'];
     Image_actin = imread(Actin_file);
-    cd('../');
-    Path = ['borders/', num2str(Number),'/'];
+    cd(b_dir);
+    Path = [b_dir, '/', num2str(Number),'/'];
     cd(Path);
     Image_borders = imread('tracked_bd.png');
-    cd('../../');
     
-    cd('actin-project/script/');
+    %% Running analysis script
     actin_analysis_v1;
-    cd('../../');
- 
-    cd('distribution/');
+    
+    %% writing down distributions in each image and analysed image
+    cd(dist_dir);
     gradient_filename = [num2str(Number),'_distribution.csv'];
     csvwrite(gradient_filename, m_added_norm);
-    cd('../');
     
-    cd('images_analysed/');
+    cd(im_dir);
     image_filename = [num2str(Number),'_analysed_image.tif'];
     print(image1, '-dtiff', '-r150', image_filename);
-    cd('../');
     
-    cd('actin-project/script/');
+    %% Von-Mises fit
     vonmises_fit_dist_sum;
-    cd('../../');
     
-    cd('summary/');
+    %% Writing down summarised data
+    
     summary = zeros(length(SD),8);
     for counter2 = 1:length(SD)
         summary(counter2,1) = counter2;
     end
     summary(:,2) = mts_density';
     summary(:,3) = SD';
-    summary(:,4) = mu_degrees';
+    summary(:,4) = mu';
     summary(:,5) = cell_data(:,2);
     summary(:,6) = cell_data(:,3);
     summary(:,7) = cell_data(:,4);
@@ -65,10 +84,14 @@ for loop=1:length(files);
     summary(:,9) = mts_area';
     
     summary_filename = [num2str(Number),'_summary.csv'];
-    headers = {'Cell', 'Density', 'SD', 'Direction_actin','Area', 'Eccentricity', 'Dorection_cell','DEV', 'Signal Area'};
+    headers = {'Cell', 'Density', 'SD', 'Direction_cytoskeleton','Area', 'Eccentricity', 'Dorection_cell','DEV', 'Signal Area'};
+    cd(sum_dir);
     csvwrite_with_headers(summary_filename,summary,headers);
-    cd('../');
     close all
 end
 
-cd('actin-project/script/');
+cd(currdir);
+
+clc
+clear variables
+close all
--- vonmises_fit_dist_sum.m
+++ vonmises_fit_dist_sum.m
@@ -2,24 +2,27 @@
 
 clear y_forfit1 y_forfit x y bin counter kappa SD mu p alpha mu_degrees
 
+%% Assign memory
 stretch_factor = 0.5;
-
 y_forfit1 = m_added_norm;
+counter = size(y_forfit1);
+y_forfit = zeros(1,counter(2)-1);
+kappa = zeros(1,counter(2)-1);
+SD = zeros(1,counter(2)-1);
+mu = zeros(1,counter(2)-1);
 x = y_forfit1(:,1);
 x = x /90 * pi;
 bin = 0.14;
-counter = size(y_forfit1);
 
+%% Von Mises Fit
 for fit=2:counter(2)
 y_forfit = y_forfit1(:,fit);
-%% Von Mises Fit
 y = y_forfit;
 kappa(fit-1) = circ_kappa(x,y,bin);
 SD(fit-1) = sqrt(1/kappa(fit-1)) * (180/pi) * stretch_factor;
 mu(fit-1) = circ_mean(x,y);
-[p alpha] = circ_vmpdf(x,mu(fit-1),kappa(fit-1));
-mu_degrees(fit-1) = mu(fit-1) * (180/pi) * stretch_factor;
-
+[p, alpha] = circ_vmpdf(x,mu(fit-1),kappa(fit-1));
+mu(fit-1) = mu(fit-1) * (180/pi) * stretch_factor;
 end
 
 
