5ace88cafb562cc4eea60708581f28025c7231a1
nbul <nbul001@gmail.com>
2017-08-01 09:52:02 +0100

    binning
    binning improved

--- allandbinned.m
+++ allandbinned.m
@@ -14,7 +14,7 @@ cd(dir_MTSD);
 files = dir('*.csv');
 Averages_MTSDall = zeros(1,2);
 data_k = zeros(1,2);
-Averages_MTSDbinned = zeros(1,4);
+Averages_MTSDbinned = zeros(1,5);
 
 for i=1:length(files)
     Name = files(i).name;
@@ -31,22 +31,27 @@ cd(sum_dir);
 csvwrite('MTSDall.scv', Averages_MTSDall);
 
 for k=0.92:0.005:0.99
-    data_k =  Averages_MTSDall( Averages_MTSDall(:,1) >= k-0.005 & Averages_MTSDall(:,1) < k+0.005,:);
+    data_k =  Averages_MTSDall( Averages_MTSDall(:,1) >= k-0.0025 & Averages_MTSDall(:,1) < k+0.0025,:);
     N = int8((k-0.915)/0.005);
-    Averages_MTSDbinned(N,1) = k;
+    Averages_MTSDbinned(N,1) = 0;
     Averages_MTSDbinned(N,2) = 0;
     Averages_MTSDbinned(N,3) = 0;
+    Averages_MTSDbinned(N,4) = 0;
     if size(data_k,1)>1
-        Averages_MTSDbinned(N,2) = mean(data_k(:,2));
-        Averages_MTSDbinned(N,3) = sqrt(var(data_k(:,2))/length(data_k(:,2)));
+        Averages_MTSDbinned(N,1) = mean(data_k(:,1));
+        Averages_MTSDbinned(N,2) = sqrt(var(data_k(:,1))/length(data_k(:,1)));
+        Averages_MTSDbinned(N,3) = mean(data_k(:,2));
+        Averages_MTSDbinned(N,4) = sqrt(var(data_k(:,2))/length(data_k(:,2)));
     elseif size(data_k,1)==1
-        Averages_MTSDbinned(N,2) = data_k(:,2);
-        Averages_MTSDbinned(N,3) = 0;
+        Averages_MTSDbinned(N,1) = data_k(:,1);
+        Averages_MTSDbinned(N,2) = 0;
+        Averages_MTSDbinned(N,3) = data_k(:,2);
+        Averages_MTSDbinned(N,4) = 0;
     end
-    Averages_MTSDbinned(N,4) = size(data_k,1);
+    Averages_MTSDbinned(N,5) = size(data_k,1);
 end
 
-headers = {'Eccentricity', 'MTSD', 'sem','cells'};
+headers = {'Eccentricity','sem', 'MTSD', 'sem','cells'};
 csvwrite_with_headers('MTSD_binned.csv',Averages_MTSDbinned,headers);
 cd(currdir);
 clear variables
