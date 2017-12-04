79b55e8fd8742ca8552b9ece803e81851f2d8531
nbul <nbul001@gmail.com>
2017-12-04 18:24:00 +0000

    postcluster
    improved obtaining pulled data in post cluster analysis

--- PostCluster/summariespostcluster.m
+++ PostCluster/summariespostcluster.m
@@ -268,134 +268,32 @@ end
 m_added_norm = zeros(45,16);
 m_added_norm(:,1) = MTSDpulled(:,1);
 Binnednumber = zeros(15,1);
+Binecc = zeros(15,3);
 for k=0.92:0.005:0.99
     N = int8((k-0.915)/0.005);
+    Binecc_temp = zeros(1,1);
     for i = 1:length(Eccpulled)
        if  Eccpulled(i)>k-0.0025 && Eccpulled(i)<k+0.0025
            m_added_norm(:,N+1) = m_added_norm(:,N+1) + MTSDpulled(:,i+1);
            Binnednumber(N,1) = Binnednumber(N,1) + 1;
+           Binecc_temp(Binnednumber(N,1),1) = Eccpulled(i);
        end
     end
+    Binecc(N,1) = mean(Binecc_temp(:,1));
+    Binecc(N,2) = sqrt(var(Binecc_temp(:,1))/Binnednumber(N,1));
+    Binecc(N,4) = Binnednumber(N,1);
+    m_added_norm(:,N+1) = m_added_norm(:,N+1)/Binnednumber(N,1);
 end
 
 vonmises_fit_dist_sum;
-result(:,1) = (0.92:0.005:0.99)';
-result(:,2) = SD';
-result(isnan(result(:,2)) == 1,:) = [];
+Binecc(:,3) = SD';
+Binecc(isnan(Binecc(:,3)) == 1,:) = [];
 
 cd(sum_dir);
-headers = {'Eccentricity','MTSD'};
-csvwrite_with_headers('MTSDpulled.csv',result,headers);
+headers = {'Eccentricity','sem','MTSD', 'N'};
+csvwrite_with_headers('MTSDpulled.csv',Binecc,headers);
 
-%% Pulled but not corrected for cell orientation
-cd(dist_dir);
-MTSDpulled(:,1) = csvread('1_distribution.csv',0,0, [0 0 44 0]);
-Eccpulled = zeros(1,1);
-counter_cells = 0;
-%% Parameters
-bin_size = 4;
-binrange = -90 : bin_size : 90;
-bincenter=binrange(1:(end-1)) + bin_size/2;
-Gx = [-2 -1 0 1 2;-3 -2 0 2 3;-4 -3 0 3 4;-3 -2 0 2 3;-2 -1 0 1 2];
-Gy = Gx';
-for loop=1:numel(files)
-    
-    %% reading files
-    
-    clear Name Number Actin_file Image_actin Path  Image_borders
-    cd(image_dir2);
-    Number = loop;
-    Actin_file = [num2str(Number),'.tif'];
-    Image = imread(Actin_file);
-    cd(b_dir);
-    Path = [b_dir, '/', num2str(Number),'/'];
-    cd(Path);
-    Image_borders = imread('tracked_bd.png');
-    [im_x, im_y] = size(Image);
-    
-    %% Collect data about cells and boundaries
-    borders;
-    
-    image_original_double_MTSD = double(im2uint16(Image));
-    
-    for k = 1:numel(b_valid)
-        
-        %Apply Sobel Filter over a MTs image to test it
-        clear H_full V_full H V M D x y mxd_thr mxd_corrected mxd_indexed
-        object_double = image_original_double_MTSD .*...
-            poly2mask(b_valid{k}(:,2),b_valid{k}(:,1),im_x,im_y);
-        H_full = conv2(object_double,Gx);
-        V_full = conv2(object_double,Gy);
-        H = H_full(5:im_x,5:im_y);
-        V = V_full(5:im_x,5:im_y);
-        M = sqrt(H.^2 + V.^2);
-        D = -(180/pi) * atan2(V, H);
-        
-        [x, y] = size(M);
-        p = 1;
-        mxd = zeros(1,2);
-        
-        for j = 2:(y-1)
-            for i=2:(x-1)
-                %Only directions different to zero are added to table
-                if ((M(i,j)) & (M(i+1,j)) & (M(i-1,j)) & (M(i,j+1))  & (M(i,j-1) ~=0) &...
-                        (M(i-1,j-1)) & (M(i-1 , j+1)) & (M(i+1 , j-1)) & (M(i+1, j+1))) ~= 0
-                    mxd(p,2) = M(i,j); %Second column with magnitudes
-                    mxd(p,1) = D(i,j); %First column with angles
-                    p = p + 1;
-                end
-            end
-        end
-        max_mxd  = max(mxd(:,2)); %maximum magnitude
-        mxd_thr = mxd./repmat([1,max_mxd], length(mxd), 1); %normalised to max magnitude
-        
-        % Remove all pixels with magnitude less than 22% of maximum
-        
-        mxd_corrected = mxd_thr(mxd_thr(:,2) >= 0.22,:);
-        mxd_corrected(mxd_corrected(:,1) < 0,1) = mxd_corrected(mxd_corrected(:,1) < 0,1) + 180;
-        mxd_corrected(:,1) = mxd_corrected(:,1) - 90;
-        mxd_corrected(mxd_corrected(:,1) >= 90,1) = 89.9;
-        mxd_corrected = sortrows(mxd_corrected,1);
-        
-        % Make histogram
-        [N, bins] = histc(mxd_corrected(:,1),binrange);
-        
-        mxd_indexed(:,2) = mxd_corrected(:,2);
-        mxd_indexed(:,1) = bins(:,1);
-        
-        
-        % Make distribution
-        m_added = zeros(45,1);
-        for i=1:(length(binrange)-1)
-            m_added(i,1) = sum(mxd_indexed(mxd_indexed(:,1) == i,2));
-        end
-        counter_cells = counter_cells + 1;
-        MTSDpulled(:,counter_cells+1) = m_added/sum(m_added);
-        Eccpulled(counter_cells) = cell_data(k,4);
-    end
-end
-m_added_norm = zeros(45,16);
-m_added_norm(:,1) = MTSDpulled(:,1);
-Binnednumber = zeros(15,1);
-for k=0.92:0.005:0.99
-    N = int8((k-0.915)/0.005);
-    for i = 1:length(Eccpulled)
-       if  Eccpulled(i)>k-0.0025 && Eccpulled(i)<k+0.0025
-           m_added_norm(:,N+1) = m_added_norm(:,N+1) + MTSDpulled(:,i+1);
-           Binnednumber(N,1) = Binnednumber(N,1) + 1;
-       end
-    end
-end
 
-vonmises_fit_dist_sum;
-clear result;
-result(:,1) = (0.92:0.005:0.99)';
-result(:,2) = SD';
-result(isnan(result(:,2)) == 1,:) = [];
-
-cd(sum_dir);
-headers = {'Eccentricity','MTSD'};
-csvwrite_with_headers('MTSDpullednotcorrected.csv',result,headers);
 
 cd(currdir);
 clc
